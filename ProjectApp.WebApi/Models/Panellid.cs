7295a4a7f3e94ef28609045fd30e675e539ef0ce
Sylvan E <Account.ander@hotmail.com>
2023-12-15 07:44:46 +0100

    Added Voogd

--- ProjectApp.WebApi/Models/Panellid.cs
+++ ProjectApp.WebApi/Models/Panellid.cs
@@ -4,10 +4,10 @@ namespace ProjectApp.WebApi.Models;
 public class Panellid : Gebruiker {
     public string? Voornaam {get; set;}
     public string? Achternaam {get; set;}
-    public List<Beperking> Beperkingen {get; set;}
-    public List<String> Hulpmiddelen {get; set;}
+    public List<Beperking> Beperkingen {get; set;} = [];
+    public List<String> Hulpmiddelen {get; set;} = [];
     public string? Benaderingsvoorkeur {get; set;}
     public bool Onafhankelijk {get; set;}
     
-    public Voogd Voogd {get; set;}
+    public Voogd? Voogd {get; set;}
 }
---
+++ ProjectApp.WebApi/Models/Voogd.cs
@@ -0,0 +1,9 @@
+namespace ProjectApp.WebApi.Models;
+
+public class Voogd {
+    public Guid Id {get; set;}
+    public Guid PanellidId {get; set;}
+    public string? Naam {get; set;}
+    public int Telefoonnummer {get; set;}
+    public string? Emailadres {get; set;}
+}
