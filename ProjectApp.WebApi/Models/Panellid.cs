d3540fa0ecc90bbb864d7c2be229c6189d76f51f
Sylvan E <Account.ander@hotmail.com>
2023-12-15 07:38:13 +0100

    Added Sub-users

---
+++ ProjectApp.WebApi/Models/Panellid.cs
@@ -0,0 +1,13 @@
+namespace ProjectApp.WebApi.Models;
+
+//->> misschien extra constraints nodig om ID consistent te houden tussen gebruiker overervingen?
+public class Panellid : Gebruiker {
+    public string? Voornaam {get; set;}
+    public string? Achternaam {get; set;}
+    public List<Beperking> Beperkingen {get; set;}
+    public List<String> Hulpmiddelen {get; set;}
+    public string? Benaderingsvoorkeur {get; set;}
+    public bool Onafhankelijk {get; set;}
+    
+    public Voogd Voogd {get; set;}
+}
